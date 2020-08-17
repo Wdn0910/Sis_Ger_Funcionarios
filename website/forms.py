56d95005789def8e7a6c2231913cc37dec80076a
Wanderson Alves <alves.wanderson@outlook.com>
2020-08-17 11:53:33 -0300

    Sis_Ger_Funcionarios em sem modificaçoes

---
+++ website/forms.py
@@ -0,0 +1,27 @@
+from helloworld.models import Funcionario
+from django import forms
+
+
+class InsereFuncionarioForm(forms.ModelForm):
+
+    chefe = forms.BooleanField(
+        label='Chefe?',
+        required=False,
+    )
+
+    biografia = forms.CharField(
+        label='Biografia',
+        required=False,
+        widget=forms.Textarea
+    )
+
+    class Meta:
+        model = Funcionario
+
+        fields = [
+            'nome',
+            'sobrenome',
+            'cpf',
+            'tempo_de_servico',
+            'remuneracao'
+        ]
---
+++ website/views.py
@@ -0,0 +1,46 @@
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView
+from helloworld.models import Funcionario
+from website.forms import InsereFuncionarioForm
+
+# PÁGINA PRINCIPAL
+
+
+class IndexTemplateView(TemplateView):
+    template_name = "website/index.html"
+
+# lISTA DE FUNCIONARIOS
+
+
+class FuncionarioListView(ListView):
+    template_name = "website/lista.html"
+    model = Funcionario
+    context_object_name = "funcionarios"
+
+# CADASTRAMENTO DE FUNCIONÁRIOS
+
+
+class FuncionarioCreateView(CreateView):
+    template_name = "website/cria.html"
+    model = Funcionario
+    form_class = InsereFuncionarioForm
+    success_url = reverse_lazy("website:lista_funcionarios")
+
+# ATUALIZAÇÃO DE FUNCIONARIOS
+
+
+class FuncionarioUpdateView(UpdateView):
+    template_name = "website/atualiza.html"
+    model = Funcionario
+    fields = '__all__'
+    context_object_name = 'funcionario'
+    success_url = reverse_lazy("website:lista_funcionarios")
+
+# EXCLUSÃO DE FUNCIONÁRIOS
+
+
+class FuncionarioDeleteView(DeleteView):
+    template_name = "website/exclui.html"
+    model = Funcionario
+    context_object_name = 'funcionario'
+    success_url = reverse_lazy("website:lista_funcionarios")
